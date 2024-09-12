e49cd9c04eab468a1160f71eb7ba9d02478fd65e
Purva Surve <113324497+purvaS98@users.noreply.github.com>
2024-09-11 20:55:16 -0700

    Time: 319 ms (92.95%) | Memory: 0B (100.00%) - LeetSync

---
+++ 197-rising-temperature/rising-temperature.sql
@@ -0,0 +1,8 @@
+# Write your MySQL query statement below
+SELECT temp1.id
+
+FROM Weather AS temp1
+
+
+JOIN Weather AS temp2 ON temp1.recordDate = DATE_ADD(temp2.recordDate, INTERVAL 1 DAY)
+WHERE temp1.temperature > temp2.temperature;
