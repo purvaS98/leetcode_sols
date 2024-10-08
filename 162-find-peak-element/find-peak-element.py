1b2c06e4943949adb0f461aeb935640dee6e713b
Purva Surve <113324497+purvaS98@users.noreply.github.com>
2024-10-08 13:05:57 -0700

    Time: 45 ms (66.25%) | Memory: 16.7 MB (26.62%) - LeetSync

---
+++ 162-find-peak-element/find-peak-element.py
@@ -0,0 +1,25 @@
+class Solution:
+    def findPeakElement(self, nums: List[int]) -> int:
+        array_size = len(nums)
+        
+        # Handle edge cases
+        if array_size == 1:
+            return 0
+        
+        if array_size == 2:
+            return 0 if nums[0] > nums[1] else 1
+
+        for i in range(1, array_size - 1):
+            if nums[i] > nums[i - 1] and nums[i] > nums[i + 1]:
+                return i
+            
+        # last element
+        if nums[-1] > nums[-2]:
+            return array_size - 1
+
+        # first element
+        if nums[0] > nums[1]:
+            return 0
+            
+        
+        
